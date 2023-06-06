d3182ce8d9328729f3a7776b1816e088771934e1
Farid Asadpour <84594007+yani5752000@users.noreply.github.com>
2023-06-06 17:36:32 -0400

    asinc4

--- cats/asyncBreeds.js
+++ cats/asyncBreeds.js
@@ -1,29 +1,36 @@
-// asyncBreeds.js
-const fs = require('fs');
-let output;
+// // asyncBreeds.js
+// const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, callback) {
-  
-  console.log('breedDetailsFromFile: Calling readFile...');
-  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    //if (!error) return data;
-    // if(!error) {
-    //     cosole.log("here is the data");
-    //     console.log(data);
-    // }
-    output = data;
-    callback();
-  });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
-};
+// const breedDetailsFromFile = function(breed) {
+//   let output;
+//   console.log('breedDetailsFromFile: Calling readFile...');
+//   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+//     console.log("In readFile's Callback: it has the data.");
+//     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+//     if (!error) {
+//       output = data;
+//       console.log("data is: ", data);
+//     }
+//   });
+//   console.log("output: ", output);
+//   // ISSUE: Attempting to return data out here will also not work.
+//   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+// };
 
-// we try to get the return value
+// // we try to get the return value
 // const bombay = breedDetailsFromFile('Bombay');
 // console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
-breedDetailsFromFile('Bombay', () => {
-    console.log("hi now");
-    console.log(output);
-});
+
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+// export the function
+module.exports = breedDetailsFromFile;
